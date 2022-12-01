dd65a6b27e2059348659dec9f6fe51fa00ecc488
Aleksandr Meshkov <meshkov88@gmail.com>
2022-12-02 06:04:59 +0700

    JS.While

--- js/script.js
+++ js/script.js
@@ -1,25 +1,12 @@
 "use strict";
-let expr = 'Peanapple';
 
-switch (expr) {
-    case "Oranges":
-      console.log("Oranges are $0.59 a pound.");
-      break;
-    case "Apples":
-      console.log("Apples are $0.32 a pound.");
-      break;
-    case "Bananas":
-      console.log("Bananas are $0.48 a pound.");
-      break;
-    case "Cherries":
-      console.log("Cherries are $3.00 a pound.");
-      break;
-    case "Mangoes":
-    case "Papayas":
-      console.log("Mangoes and papayas are $2.79 a pound.");
-      break;
-    default:
-      console.log("Sorry, we are out of " + expr + ".");
-  }
-  
-  console.log("Is there anything else you'd like?");
+const printNumbers = (initialNumber) => {
+    // BEGIN (write your solution here)
+    let i = initialNumber;
+    while (i > 0) {
+        console.log(i);
+      i--;
+    }
+};
+
+printNumbers(5);
