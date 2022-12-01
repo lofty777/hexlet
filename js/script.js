7e00377e0d5f96445c38912a2a3851c21a361638
Aleksandr Meshkov <meshkov88@gmail.com>
2022-12-01 14:12:27 +0700

    JS. Switch

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 "use strict";
+let expr = 'Peanapple';
 
-const normalizeUrl = (site) => {
-    let normalizedUrl;
-    if (site.startsWith('https://')) {
-      normalizedUrl = site;
-    } else {
-      normalizedUrl = `https://${site}`;
-    }
+switch (expr) {
+    case "Oranges":
+      console.log("Oranges are $0.59 a pound.");
+      break;
+    case "Apples":
+      console.log("Apples are $0.32 a pound.");
+      break;
+    case "Bananas":
+      console.log("Bananas are $0.48 a pound.");
+      break;
+    case "Cherries":
+      console.log("Cherries are $3.00 a pound.");
+      break;
+    case "Mangoes":
+    case "Papayas":
+      console.log("Mangoes and papayas are $2.79 a pound.");
+      break;
+    default:
+      console.log("Sorry, we are out of " + expr + ".");
+  }
   
-    return normalizedUrl;
-  };
-
-console.log(normalizeUrl('http://;adjf;j'));
+  console.log("Is there anything else you'd like?");
