7c9d4cb6ffae60942c0dd070d536850fd076d55b
Aleksandr Meshkov <meshkov88@gmail.com>
2022-12-02 06:24:33 +0700

    JS.HW.While

--- js/script.js
+++ js/script.js
@@ -7,6 +7,19 @@ const printNumbers = (initialNumber) => {
         console.log(i);
       i--;
     }
+    console.log('finished!');
 };
 
 printNumbers(5);
+
+const multiplyNumbersFromRange = (start, finish) => {
+    let i = start;
+    let multiply = 1;
+    while (i <= finish) {
+      multiply *= i;
+      i += 1;
+    }
+    return multiply;
+  };
+  
+  console.log(multiplyNumbersFromRange(3, 9)); // 60
