a0d87512122d58d3701c0b1613a01acc1b6ccc72
Aleksandr Meshkov <meshkov88@gmail.com>
2022-12-05 05:42:42 +0700

    Array.HW.getWeekends

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
 "use strict";
 
-const encrypt = (message) => {
-    let result = '';
+const getWeekends = (format) => {
+    const shortweekends = ['sat', 'sun'];
+    const longweekends = ['saturday', 'sunday'];
     
-    for (let i = 0; i < message.length; i+= 2) {
-        const nextSymbol = message[i+1] || '';
-       
-        result = `${result}${nextSymbol}${message[i]}`;
-    }
-    
-    return result;
-  };
+    if (format === 'short') {
+        return shortweekends;
+    } 
+     return longweekends;
+};
 
-
-
-
+console.log(getWeekends('short'));
