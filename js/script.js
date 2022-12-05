9955791b5a46906ae5e64540a295de9e7df1af32
Aleksandr Meshkov <meshkov88@gmail.com>
2022-12-06 05:10:35 +0700

    Arrays.HW.addPrefix

--- js/script.js
+++ js/script.js
@@ -9,13 +9,30 @@ delete animals[3];
 
 console.log(animals[0]);
 
-const get = (array, index, param = null) => {
-    if (index < array.length && index > -1) {
-        return array[index];
+const addPrefix = (array, prefix) => {
+    const array2 = array.slice();
+    for (let i = 0; i < array2.length; i += 1) {
+        array2[i] = `${prefix} ${array[i]}`;
     }
-    return param;
+    return array2;
 };
-    const cities = ['moscow', 'london', 'berlin', 'porto'];
 
-    console.log(get(cities, 0));
+//Second solution
+
+// const addPrefix = (array, prefix) => {
+//     const array2 = [];
+//     for (let i = 0; i < array.length; i += 1) {
+//       array2[i] = `${prefix} ${array[i]}`;
+//     }
+//     return array2;
+//   };
+  
+//   export default addPrefix;
+
+const names = ['john', 'alex', 'polina', 'teo'];
+const newNames1 = addPrefix(names, 'Mr');
+
+console.log(newNames1);
+console.log(names);
+
 
