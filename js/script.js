65b1f813812b353b0611cc58052d7466a1c7f87a
Aleksandr Meshkov <meshkov88@gmail.com>
2022-12-08 20:51:18 +0700

    Array.HW.countUniqChars

--- js/script.js
+++ js/script.js
@@ -1,35 +1,22 @@
 "use strict";
+import _ from 'lodash';
 
-// const makeCensored = (text, stopWords) => {
-//     const separator = ' ';
-//     const sensore = '$#%!';
-//     const words = text.split(separator);
-//     // console.log(words);
-//     for (let i = 0; i < stopWords.length; i++) {
-        
-//         for (let j = 0; j < words.length; j++) {
-//             if (stopWords[i] === words[j]) {
-//                 words[j] = sensore;
-//             }
-//         }
-//     }
-//     console.log(words.join(separator));
-// };
+const text1 = "yyab";
 
-// Second solution
-const makeCensored = (text, stopWords) => {
-    const separator = ' ';
-    const sensore = '$#%!';
-    const words = text.split(separator);
-  
-    const result = [];
-    for (const word of words) {
-      const newWord = stopWords.includes(word) ? sensore : word;
-      result.push(newWord);
+const countUniqChars = (text) => {
+    const coll = [];
+    for (const item of text) {
+        // coll.push(item);
+        if (!coll.includes(item)) {
+            coll.push(item);
+        }
     }
-  
-    console.log(result.join(separator)) ;
-  };
-  
-const text = 'chicken chicken? chicken! chicken';
-makeCensored(text, ['?', 'chicken']);
+    // const result = _.uniq(coll);
+    return coll.length;
+};
+
+export default countUniqChars;
+
+console.log(countUniqChars(text1));
+
+
--- js/test.js
+++ js/test.js
@@ -1,23 +1,30 @@
-const field = [];
-for (let i = 1; i < 4; i++) {
-    field.push([null,null,null]);
-    // for (let j = 0; j < 3; j++) {
-    //     field[j] = [null, null, null];
-    // }
-}
-console.log(field);
+import _ from 'lodash';
+// var numbers = [1, 2, 2, 4];
+// var result = _.uniq(numbers);
+// console.log(result);
 
-field [1] [2] = 'x'
-console.log(field);
+const text1 = "yyab";
+// const arr1 = [];
+// for (let i = 0; i < text1.length; i++) {
+//     arr1.push(text1[i]);
+// }
+// console.log(arr1);
+// const result = _.uniq(arr1);
+// console.log(result);
+// console.log(result.length);
 
-const hasPlayerMove = (field, symbol) => {
-    for (const row of field) {
-        if (row.includes(symbol)) {
-            return true;
+const countUniqChars = (text) => {
+    const coll = [];
+    for (const item of text) {
+        // coll.push(item);
+        if (!coll.includes(item)) {
+            coll.push(item);
         }
     }
-
-    return false;
+    // const result = _.uniq(coll);
+    return coll.length;
 };
 
-console.log(hasPlayerMove(field, 'd'));
+export default countUniqChars;
+
+console.log(countUniqChars(text1));
